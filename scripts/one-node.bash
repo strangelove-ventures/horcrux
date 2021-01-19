859f6e5f3a22f9a4c8e4015577c51295e3450915
Jack Zampolin <jack.zampolin@gmail.com>
2021-01-19 09:56:44 -0800

    Push main @jackzampolin: update one-node.bash

--- scripts/one-node.bash
+++ scripts/one-node.bash
@@ -14,7 +14,7 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-echo "Creating akashd instance with home=$CHAINDIR chain-id=$CHAINID..."
+echo "Creating gaiad instance with home=$CHAINDIR chain-id=$CHAINID..."
 # Build genesis file incl account for passed address
 coins="100000000000stake,100000000000samoleans"
 
@@ -30,20 +30,20 @@ n1cfg="$n1cfgDir/config.toml"
 kbt="--keyring-backend="test""
 
 # Initialize the 2 home directories
-akashd $home0 --chain-id $CHAINID init n0 &>/dev/null
-akashd $home1 --chain-id $CHAINID init n1 &>/dev/null
+gaiad $home0 --chain-id $CHAINID init n0 #&>/dev/null
+gaiad $home1 --chain-id $CHAINID init n1 #&>/dev/null
 
 # Add some keys for funds
-akashctl $home0 keys add validator $kbt &>/dev/null
-akashctl $home0 keys add extra $kbt &>/dev/null
+gaiad $home0 keys add validator $kbt #&>/dev/null
+gaiad $home0 keys add extra $kbt #&>/dev/null
 
 # Add addresses to genesis
-akashd $home0 add-genesis-account $(akashctl $home0 keys $kbt show validator -a) $coins &>/dev/null
-akashd $home0 add-genesis-account $(akashctl $home0 keys $kbt show extra -a) $coins &>/dev/null
+gaiad $home0 add-genesis-account $(gaiad $home0 keys $kbt show validator -a) $coins #&>/dev/null
+gaiad $home0 add-genesis-account $(gaiad $home0 keys $kbt show extra -a) $coins #&>/dev/null
 
 # Finalize genesis on n0 node
-akashd $home0 gentx --home-client $n0dir --name validator $kbt &>/dev/null
-akashd $home0 collect-gentxs &>/dev/null
+gaiad $home0 gentx validator 100000000000stake --home $n0dir $kbt --chain-id $CHAINID #&>/dev/null
+gaiad $home0 collect-gentxs #&>/dev/null
 
 # Copy genesis over to n1
 cp $n0cfgDir/genesis.json $n1cfgDir/genesis.json
@@ -62,8 +62,8 @@ sed -i '' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' $n1cfg
 sed -i '' 's#priv_validator_laddr = ""#priv_validator_laddr = "tcp://0.0.0.0:1235"#g' $n1cfg
 
 # Set peers for both nodes
-peer0="$(akashd $home0 tendermint show-node-id)@127.0.0.1:26656"
-peer1="$(akashd $home1 tendermint show-node-id)@127.0.0.1:26666"
+peer0="$(gaiad $home0 tendermint show-node-id)@127.0.0.1:26656"
+peer1="$(gaiad $home1 tendermint show-node-id)@127.0.0.1:26666"
 sed -i '' 's#persistent_peers = ""#persistent_peers = "'$peer1'"#g' $n0cfg
 sed -i '' 's#persistent_peers = ""#persistent_peers = "'$peer0'"#g' $n1cfg
 
@@ -72,5 +72,5 @@ mv $n0cfgDir/priv_validator_key.json $HOME/.tmsigner/priv_validator_key.json
 mv $n0dir/data/priv_validator_state.json $HOME/.tmsigner/data/${CHAINID}_priv_validator_state.json
 
 # Start the akash instances
-akashd $home0 start --pruning=nothing > $CHAINDIR/$CHAINID.n0.log 2>&1 &
-akashd $home1 start --pruning=nothing > $CHAINDIR/$CHAINID.n1.log 2>&1 &
+gaiad $home0 start --pruning=nothing > $CHAINDIR/$CHAINID.n0.log 2>&1 &
+gaiad $home1 start --pruning=nothing > $CHAINDIR/$CHAINID.n1.log 2>&1 &
