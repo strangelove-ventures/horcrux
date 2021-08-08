c2be5d6b10223088bbd8c7f70ad8c7e2fd8bef9a
Jack Zampolin <jack.zampolin@gmail.com>
2021-08-07 17:17:51 -0700

    Merge PR #16: Automated Releases
    * WIP
    
    * Version command working as intended
    
    * Builds working with some caveats
    
    * rename signer files and golang ci lint

--- Makefile
+++ Makefile
@@ -1,24 +1,39 @@
-all: build
+VERSION := $(shell echo $(shell git describe --tags) | sed 's/^v//')
+SDKVERSION := $(shell go list -m -u -f '{{.Version}}' github.com/cosmos/cosmos-sdk)
+TMVERSION := $(shell go list -m -u -f '{{.Version}}' github.com/tendermint/tendermint)
+COMMIT  := $(shell git log -1 --format='%H')
+
+all: install
+
+LD_FLAGS = -X github.com/strangelove-ventures/horcrux/cmd/horcrux/cmd.Version=$(VERSION) \
+	-X github.com/strangelove-ventures/horcrux/cmd/horcrux/cmd.Commit=$(COMMIT) \
+	-X github.com/strangelove-ventures/horcrux/cmd/horcrux/cmd.SDKVersion=$(SDKVERSION) \
+	-X github.com/strangelove-ventures/horcrux/cmd/horcrux/cmd.TMVersion=$(TMVERSION)
+
+BUILD_FLAGS := -ldflags '$(LD_FLAGS)'
 
 build:
-	@go build --mod readonly -o build/ ./cmd/horcrux/...
+	@go build -mod readonly $(BUILD_FLAGS) -o build/ ./cmd/horcrux/...
 
 install:
-	@go install ./cmd/horcrux/...
+	@go install -mod readonly $(BUILD_FLAGS) ./cmd/horcrux/...
 
 build-linux:
-	@GOOS=linux GOARCH=amd64 go build -o ./build/horcrux ./cmd/horcrux
+	@GOOS=linux GOARCH=amd64 go build --mod readonly $(BUILD_FLAGS) -o ./build/horcrux ./cmd/horcrux
 
 test:
-	@go test -v ./...
+	@go test -mod readonly -v ./...
 
 clean:
 	rm -rf build
 
 build-simd-docker:
-	docker build -t jackzampolin/simd:v0.42.3 -f ./docker/simd/Dockerfile ./docker/simd/
+	docker build -t jackzampolin/simd:$(SDKVERSION) --build-arg VERSION=$(SDKVERSION) -f ./docker/simd/Dockerfile ./docker/simd/
 
 build-horcrux-docker:
-	docker build -t strangelove-ventures/horcrux:v0.1.0 -f ./docker/horcrux/Dockerfile .
+	docker build -t jackzampolin/horcrux:$(VERSION) -f ./docker/horcrux/Dockerfile .
+
+push-simd-docker:
+	docker push jackzampolin/simd:$(SDKVERSION)
 
 .PHONY: all lint test race msan tools clean build
---
+++ cmd/horcrux/cmd/version.go
@@ -0,0 +1,72 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	// application's version string
+	Version = ""
+	// commit
+	Commit = ""
+	// sdk version
+	SDKVersion = ""
+	// tendermint version
+	TMVersion = ""
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+// Info defines the application version information.
+type Info struct {
+	Version           string `json:"version" yaml:"version"`
+	GitCommit         string `json:"commit" yaml:"commit"`
+	GoVersion         string `json:"go_version" yaml:"go_version"`
+	CosmosSdkVersion  string `json:"cosmos_sdk_version" yaml:"cosmos_sdk_version"`
+	TendermintVersion string `json:"tendermint_version" yaml:"tendermint_version"`
+}
+
+func NewInfo() Info {
+	return Info{
+		Version:           Version,
+		GitCommit:         Commit,
+		GoVersion:         fmt.Sprintf("%s %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH),
+		CosmosSdkVersion:  SDKVersion,
+		TendermintVersion: TMVersion,
+	}
+}
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "version information for horcrux",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		bz, err := json.MarshalIndent(NewInfo(), "", "  ")
+		if err != nil {
+			return err
+		}
+		cmd.Println(string(bz))
+		return nil
+	},
+}
