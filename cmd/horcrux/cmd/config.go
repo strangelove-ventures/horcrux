9c2c4ab15c21d30de02b6eda769670ac62bc6dae
freshfab <fabian.bockaj@mway.io>
2021-11-15 22:30:05 +0100

    Add peers add/remove

--- cmd/horcrux/cmd/config.go
+++ cmd/horcrux/cmd/config.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -18,11 +19,13 @@ import (
 )
 
 func init() {
-	// TODO: config nodes add/remove
 	nodesCmd.AddCommand(addNodesCmd())
 	nodesCmd.AddCommand(removeNodesCmd())
 	configCmd.AddCommand(nodesCmd)
-	// TODO: config peers add/remove
+
+	peersCmd.AddCommand(addPeersCmd())
+	peersCmd.AddCommand(removePeersCmd())
+	configCmd.AddCommand(peersCmd)
 	// TODO: config chain-id set
 	configCmd.AddCommand(setChainIdCmd)
 
@@ -268,7 +271,7 @@ func removeNodesCmd() *cobra.Command {
 
 // diffSetChainNode decribes the difference set of setA-setB, which are all ChainNodes
 // of setA that are also part of setB.
-func diffSetChainNode(setA, setB []ChainNode) (newNodes []ChainNode) {
+func diffSetChainNode(setA, setB []ChainNode) (diffSet []ChainNode) {
 	for _, b := range setB {
 		found := false
 		for _, a := range setA {
@@ -277,7 +280,110 @@ func diffSetChainNode(setA, setB []ChainNode) (newNodes []ChainNode) {
 			}
 		}
 		if !found {
-			newNodes = append(newNodes, b)
+			diffSet = append(diffSet, b)
+		}
+	}
+	return
+}
+
+var peersCmd = &cobra.Command{
+	Use:   "peers",
+	Short: "Commands to configure the peer nodes",
+}
+
+func addPeersCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "add [peer-nodes]",
+		Aliases: []string{"a"},
+		Short:   "add peer node(s) to the cosigner's configuration",
+		Long: "add peer node(s) to the cosigner's configuration.\n\n" +
+			"[peer-nodes] is a comma seperated array of peer node addresses i.e.\n" +
+			"tcp://peer-node-1:1234,tcp://peer-node-2:1234",
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			var home string // In root.go we end up with our
+			if homeDir != "" {
+				home = homeDir
+			} else {
+				home, _ = homedir.Dir()
+				home = path.Join(home, ".horcrux")
+			}
+			if _, err := os.Stat(homeDir); !os.IsNotExist(err) {
+				return fmt.Errorf("%s is not empty, check for existing configuration and clear path before trying again", homeDir)
+			}
+
+			argPeers, err := peersFromFlag(args[0])
+			if err != nil {
+				return err
+			}
+			diffSet := diffSetCosignerPeer(config.CosignerConfig.Peers, argPeers)
+			if len(diffSet) == 0 {
+				return errors.New("no new peer nodes specified in args")
+			}
+			if err := validateCosignerPeers(diffSet); err != nil {
+				fmt.Println(err)
+			}
+			config.CosignerConfig.Peers = append(config.CosignerConfig.Peers, diffSet...)
+
+			if err := writeConfigFile(path.Join(home, "config.yaml"), config); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+}
+
+func removePeersCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "remove [peer-nodes]",
+		Aliases: []string{"r"},
+		Short:   "remove peer node(s) from the cosigner's configuration",
+		Long: "remove peer node(s) from the cosigner's configuration.\n\n" +
+			"[peer-nodes] is a comma seperated array of peer node addresses i.e.\n" +
+			"tcp://peer-node-1:1234,tcp://peer-node-2:1234",
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			var home string // In root.go we end up with our
+			if homeDir != "" {
+				home = homeDir
+			} else {
+				home, _ = homedir.Dir()
+				home = path.Join(home, ".horcrux")
+			}
+			if _, err := os.Stat(homeDir); !os.IsNotExist(err) {
+				return fmt.Errorf("%s is not empty, check for existing configuration and clear path before trying again", homeDir)
+			}
+
+			argPeers, err := peersFromFlag(args[0])
+			if err != nil {
+				return err
+			}
+			diffSet := diffSetCosignerPeer(argPeers, config.CosignerConfig.Peers)
+			if len(diffSet) == 0 {
+				return errors.New("cannot remove all peer nodes from config, please leave at least one")
+			}
+			config.CosignerConfig.Peers = diffSet
+
+			if err := writeConfigFile(path.Join(home, "config.yaml"), config); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+}
+
+// diffSetCosignerPeer decribes the difference set of setA-setB, which are all
+// CosignerPeers of setA that are also part of setB.
+func diffSetCosignerPeer(setA, setB []CosignerPeer) (diffSet []CosignerPeer) {
+	for _, b := range setB {
+		found := false
+		for _, a := range setA {
+			if b == a {
+				found = true
+			}
+		}
+		if !found {
+			diffSet = append(diffSet, b)
 		}
 	}
 	return
@@ -353,10 +459,66 @@ type CosignerPeer struct {
 	P2PAddr string `json:"p2p-addr" yaml:"p2p-addr"`
 }
 
+func readKeyShare() (*signer.CosignerKey, error) {
+	// Read ID from local key share
+	var home string // In root.go we end up with our
+	if homeDir != "" {
+		home = homeDir
+	} else {
+		home, _ = homedir.Dir()
+		home = path.Join(home, ".horcrux")
+	}
+
+	if _, err := os.Stat(homeDir); !os.IsNotExist(err) {
+		return nil, fmt.Errorf("%s is not empty, check for existing configuration and clear path before trying again", homeDir)
+	}
+
+	file, err := os.Open(path.Join(home, "share.json"))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bz, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	var key signer.CosignerKey
+	if err := json.Unmarshal(bz, &key); err != nil {
+		return nil, err
+	}
+	return &key, nil
+}
+
 func validateCosignerPeers(peers []CosignerPeer) error {
-	// TODO: check IDs to ensure none are duplicated and
-	// that the key share which is configured for the local node
-	// is the last remaining share
+	// Check IDs to make sure none are duplicated
+	encountered := make(map[int]string)
+	duplicates := []int{}
+	for _, peer := range peers {
+		if _, found := encountered[peer.ShareID]; !found {
+			encountered[peer.ShareID] = peer.P2PAddr
+		} else {
+			duplicates = append(duplicates, peer.ShareID)
+		}
+	}
+	if len(duplicates) != 0 {
+		return fmt.Errorf("found duplicates for peer IDs: %v", duplicates)
+	}
+
+	// TODO
+	// key, err := readKeyShare()
+	// if err != nil {
+	// 	return err
+	// }
+
+	// // Check that the key share which is configured for the local node
+	// // is the last remaining share
+	// for _, peer := range config.CosignerPeers() {
+	// 	if peer.ID == key.ID {
+	// 		return fmt.Errorf("conflict: local node's key share ID %v matches with another peer in the config, please make sure you're using the correct share", key.ID)
+	// 	}
+	// }
+
 	return nil
 }
 
