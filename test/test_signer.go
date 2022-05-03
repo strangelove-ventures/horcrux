07d35f856c6be7b2157ec59532f79be6632a3cdb
Andrew Gouin <andrew@gouin.io>
2022-05-03 10:36:55 -0600

    Timeouts are too long (#76)
    * Timeouts should be 30 seconds
    
    * Increase timeout to 60s for CI
    
    * 60s for test signer too

--- test/test_signer.go
+++ test/test_signer.go
@@ -9,7 +9,6 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
-	"time"
 
 	"github.com/ory/dockertest"
 	"github.com/ory/dockertest/docker"
@@ -440,7 +439,7 @@ func (ts *TestSigner) StartContainer() error {
 
 // StopContainer stops a TestSigners docker container
 func (ts *TestSigner) StopContainer() error {
-	return ts.Pool.Client.StopContainer(ts.Container.ID, uint(time.Second*30))
+	return ts.Pool.Client.StopContainer(ts.Container.ID, 60)
 }
 
 func (ts *TestSigner) StopAndRemoveContainer(force bool) error {
