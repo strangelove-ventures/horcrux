dc459d149dc5d246a4b3ae241f95a40f062ceffa
Andrew Gouin <andrew@gouin.io>
2023-04-24 16:17:07 -0600

    single signer multi chain

--- signer/single_signer_validator.go
+++ signer/single_signer_validator.go
@@ -1,42 +1,88 @@
 package signer
 
 import (
+	"fmt"
+	"os"
 	"sync"
 
-	tmcrypto "github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/privval"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	tm "github.com/tendermint/tendermint/types"
 )
-
-var _ PrivValidator = &SingleSignerValidator{}
 
 // SingleSignerValidator guards access to an underlying PrivValidator by using mutexes
 // for each of the PrivValidator interface functions
 type SingleSignerValidator struct {
-	PrivValidator tm.PrivValidator
-	pvMutex       sync.Mutex
+	config     *RuntimeConfig
+	chainState map[string]*SingleSignerChainState
+}
+
+type SingleSignerChainState struct {
+	filePV  *privval.FilePV
+	pvMutex sync.Mutex
+}
+
+func NewSingleSignerValidator(config *RuntimeConfig) *SingleSignerValidator {
+	return &SingleSignerValidator{
+		config:     config,
+		chainState: make(map[string]*SingleSignerChainState),
+	}
 }
 
 // GetPubKey implements types.PrivValidator
-func (pv *SingleSignerValidator) GetPubKey() (tmcrypto.PubKey, error) {
-	pv.pvMutex.Lock()
-	defer pv.pvMutex.Unlock()
-	return pv.PrivValidator.GetPubKey()
+func (pv *SingleSignerValidator) GetPubKey() (crypto.PubKey, error) {
+	keyFile := pv.config.KeyFilePathSingleSigner()
+	filePV := privval.LoadFilePVEmptyState(keyFile, "")
+	return filePV.GetPubKey()
 }
 
 // SignVote implements types.PrivValidator
 func (pv *SingleSignerValidator) SignVote(chainID string, vote *tmproto.Vote) error {
-	pv.pvMutex.Lock()
-	defer pv.pvMutex.Unlock()
-	return pv.PrivValidator.SignVote(chainID, vote)
+	chainState, err := pv.loadChainStateIfNecessary(chainID)
+	if err != nil {
+		return err
+	}
+	chainState.pvMutex.Lock()
+	defer chainState.pvMutex.Unlock()
+	return chainState.filePV.SignVote(chainID, vote)
 }
 
 // SignProposal implements types.PrivValidator
 func (pv *SingleSignerValidator) SignProposal(chainID string, proposal *tmproto.Proposal) error {
-	pv.pvMutex.Lock()
-	defer pv.pvMutex.Unlock()
-	return pv.PrivValidator.SignProposal(chainID, proposal)
+	chainState, err := pv.loadChainStateIfNecessary(chainID)
+	if err != nil {
+		return err
+	}
+	chainState.pvMutex.Lock()
+	defer chainState.pvMutex.Unlock()
+	return chainState.filePV.SignProposal(chainID, proposal)
 }
 
-// SignProposal implements PrivValidator
+func (pv *SingleSignerValidator) loadChainStateIfNecessary(chainID string) (*SingleSignerChainState, error) {
+	if chainState, ok := pv.chainState[chainID]; ok {
+		return chainState, nil
+	}
+
+	keyFile := pv.config.KeyFilePathSingleSigner()
+	stateFile := pv.config.PrivValStateFile(chainID)
+	var filePV *privval.FilePV
+	if _, err := os.Stat(stateFile); err != nil {
+		if !os.IsNotExist(err) {
+			panic(fmt.Errorf("failed to load state file (%s) - %w", stateFile, err))
+		}
+		// The only scenario in which we want to initialize a new state file
+		// is when the state file does not exist.
+		filePV = privval.LoadFilePVEmptyState(keyFile, stateFile)
+	} else {
+		filePV = privval.LoadFilePV(keyFile, stateFile)
+	}
+
+	chainState := &SingleSignerChainState{
+		filePV: filePV,
+	}
+	pv.chainState[chainID] = chainState
+
+	return chainState, nil
+}
+
 func (pv *SingleSignerValidator) Stop() {}
---
+++ signer/single_signer_validator_test.go
@@ -0,0 +1,116 @@
+package signer
+
+import (
+	"path/filepath"
+	"time"
+
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	tmcryptoed25519 "github.com/tendermint/tendermint/crypto/ed25519"
+	"github.com/tendermint/tendermint/crypto/tmhash"
+	tmjson "github.com/tendermint/tendermint/libs/json"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
+	tmprivval "github.com/tendermint/tendermint/privval"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tm "github.com/tendermint/tendermint/types"
+)
+
+func TestSingleSignerValidator(t *testing.T) {
+
+	tmpDir := t.TempDir()
+	stateDir := filepath.Join(tmpDir, "state")
+
+	err := os.MkdirAll(stateDir, 0700)
+	require.NoError(t, err)
+
+	runtimeConfig := &RuntimeConfig{
+		HomeDir:  tmpDir,
+		StateDir: filepath.Join(tmpDir, "state"),
+	}
+
+	privateKey := tmcryptoed25519.GenPrivKey()
+
+	marshaled, err := tmjson.Marshal(tmprivval.FilePVKey{
+		Address: privateKey.PubKey().Address(),
+		PubKey:  privateKey.PubKey(),
+		PrivKey: privateKey,
+	})
+	require.NoError(t, err)
+
+	err = os.WriteFile(runtimeConfig.KeyFilePathSingleSigner(), marshaled, 0600)
+	require.NoError(t, err)
+
+	validator := NewSingleSignerValidator(runtimeConfig)
+
+	proposal := tmproto.Proposal{
+		Height: 1,
+		Round:  20,
+		Type:   tmproto.ProposalType,
+	}
+
+	signBytes := tm.ProposalSignBytes(testChainID, &proposal)
+
+	err = validator.SignProposal(testChainID, &proposal)
+	require.NoError(t, err)
+
+	require.True(t, privateKey.PubKey().VerifySignature(signBytes, proposal.Signature))
+
+	proposal = tmproto.Proposal{
+		Height:    1,
+		Round:     20,
+		Type:      tmproto.ProposalType,
+		Timestamp: time.Now(),
+	}
+
+	// should be able to sign same proposal with only differing timestamp
+	err = validator.SignProposal(testChainID, &proposal)
+	require.NoError(t, err)
+
+	// construct different block ID for proposal at same height as highest signed
+	randHash := tmrand.Bytes(tmhash.Size)
+	blockID := tmproto.BlockID{Hash: randHash,
+		PartSetHeader: tmproto.PartSetHeader{Total: 5, Hash: randHash}}
+
+	proposal = tmproto.Proposal{
+		Height:  1,
+		Round:   20,
+		Type:    tmproto.ProposalType,
+		BlockID: blockID,
+	}
+
+	// should not be able to sign same proposal at same height as highest signed with different BlockID
+	err = validator.SignProposal(testChainID, &proposal)
+	require.Error(t, err, "double sign!")
+
+	proposal = tmproto.Proposal{
+		Height: 1,
+		Round:  19,
+		Type:   tmproto.ProposalType,
+	}
+
+	// should not be able to sign lower than highest signed
+	err = validator.SignProposal(testChainID, &proposal)
+	require.Error(t, err, "double sign!")
+
+	// lower LSS should sign for different chain ID
+	err = validator.SignProposal("different", &proposal)
+	require.NoError(t, err)
+
+	// reinitialize validator to make sure new runtime will not allow double sign
+	validator = NewSingleSignerValidator(runtimeConfig)
+
+	err = validator.SignProposal(testChainID, &proposal)
+	require.Error(t, err, "double sign!")
+
+	proposal = tmproto.Proposal{
+		Height: 1,
+		Round:  21,
+		Type:   tmproto.ProposalType,
+	}
+
+	// signing higher block now should succeed
+	err = validator.SignProposal(testChainID, &proposal)
+	require.NoError(t, err)
+}
