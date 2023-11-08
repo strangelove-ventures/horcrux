461444f0922518a6f4973f7d68a353ab13757fe1
Andrew Gouin <andrew@gouin.io>
2023-11-08 16:29:18 -0700

    Remove buf mod update

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -3,9 +3,7 @@
 set -eox pipefail
 
 echo "Generating proto code"
-cd proto
-buf mod update
-cd ..
+
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
